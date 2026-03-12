5d2a225c7f99690fee1919c4a69e640bd8d9aca9
rohit-sahu-dev <sahu.rohit2101@gmail.com>
2026-03-13 00:34:02 +0530

    new_update

---
+++ 1. Arrays/Second_Max.java
@@ -0,0 +1,24 @@
+public class Second_Max {
+    public static void main(String[] args) {
+        int[] arr = { 1, 6, 3, 8, 10, 4 };
+        int max1 = 0, max2 = 0;
+        if (arr[0] > arr[1]) {
+            max1 = arr[0];
+            max2 = arr[1];
+        } else {
+            max1 = arr[1];
+            max2 = arr[0];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] > max2) {
+                max2 = arr[i];
+            }
+
+        }
+        System.out.println("Second largest elements in this array is : " + max2);
+    }
+
+}
