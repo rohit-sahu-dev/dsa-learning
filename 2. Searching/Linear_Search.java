38d96ef9d3fe28a4ab03f67a0d6c260dc31dee4a
rohit-sahu-dev <sahu.rohit2101@gmail.com>
2026-03-28 18:09:59 +0530

    Started Searching

--- 2. Searching/Linear_Search.java
+++ 2. Searching/Linear_Search.java
@@ -1,28 +1,24 @@
 import java.util.*;
 
 public class Linear_Search {
-
-    public static int LS(int[] arr, int key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == key)
-                return i;
-        }
-        return -1;
-
-    }
-
     public static void main(String[] args) {
         int[] arr = { 11, 2, 9, 16, 25 };
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the element you want to search : ");
         int key = sc.nextInt();
-        int result = LS(arr, key);
-        if (result == -1) {
-            System.out.println("Element not found.");
-        } else {
-            System.out.println("Element found at index " + result);
+        boolean flag = false;
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                flag=true;
+                break;
+            }
         }
+        if (flag == true)
+            System.out.println("ELement found at index : "+ i);
+        else
+            System.out.println("Element not found.");
+
 
     }
-
 }
